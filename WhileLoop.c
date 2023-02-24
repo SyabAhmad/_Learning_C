ffd5efbbd312d4bc3fdf4bb3f0753876ea77984a
Syed Syab Ahmad Shah <81256221+SyabAhmad@users.noreply.github.com>
2023-02-24 18:09:31 +0500

    Add files via upload

---
+++ WhileLoop.c
@@ -0,0 +1,14 @@
+//
+// Created by syabs on 2/24/2023.
+//
+#include<stdio.h>
+int main()
+{
+    int a = 0;
+    do
+    {
+        printf("de Developer %i\n", a);
+        a++;
+    }while(a<10);
+    return 0;
+}
